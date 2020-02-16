b342f1e2b5421d3380f3e174c13f772ce107f615
Jonathan <jonatha.noack@student.unisg.ch>
2020-02-16 14:28:30 +0100

    Added Summary Function

--- script.r
+++ script.r
@@ -1,7 +1,21 @@
+install.packages("tidyverse")
+install.packages('pxR')
+library(tidyverse)
+library(dplyr)
 library(pxR)
-dataimport <- read.px('px-x-1103020200_101.px', encoding='UTF-8')
+# Enter filename down below
+dataimport <- read.px('px-x-1103020200_101.px')
 data <- as.data.frame(dataimport)
 
 write.csv(data,'pxexport.csv', fileEncoding='utf-8')
 
+personenwagen <- subset.data.frame(data, Fahrzeuggruppe....art == '> Personenwagen')
+grouped <- group_by(.data = personenwagen, ) %>% summarise(n = sum(value))
+summarise(group_by(.data = personenwagen, Jahr, Treibstoff), n = sum(value))
 
+personenwagen %>%
+  group_by(Jahr, Monat, Treibstoff) %>%
+  dplyr::summarise(n = sum(value)) -> summie
+
+
+write.csv(summie,'monthly_18_19.csv')
